956756ab046071655a16f1c08fdceb03e61e079e
Damien Brebion <mangasa2000@hotmail.com>
2020-07-26 20:17:14 +0200

    Updates

---
+++ precache-manifest.88e1269656ce41aa394b22c82017bd1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf4ab97f5ae63236c7e290fa4c78ad0f",
+    "url": "/sep-submission/index.html"
+  },
+  {
+    "revision": "08fab3372e1f41c1abd7",
+    "url": "/sep-submission/static/css/main.82472986.chunk.css"
+  },
+  {
+    "revision": "7d1424cb2061eee9e915",
+    "url": "/sep-submission/static/js/2.4206d0b2.chunk.js"
+  },
+  {
+    "revision": "bf82086dd002ea3aa1ff6bef367806cd",
+    "url": "/sep-submission/static/js/2.4206d0b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08fab3372e1f41c1abd7",
+    "url": "/sep-submission/static/js/main.96eb6a88.chunk.js"
+  },
+  {
+    "revision": "e6fd55c0cd31f174dd5b",
+    "url": "/sep-submission/static/js/runtime-main.2485fa61.js"
+  }
+]);
