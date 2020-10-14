8e896ec2cd7cde032be8b792b767729f6ce45931
JonasVervloet <vervloetjonas@hotmail.com>
2020-10-14 18:24:47 +0200

    Extract text from FormSent alert

--- src/components/alert/formSent.jsx
+++ src/components/alert/formSent.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import ShowAlert from './showAlert';
+import { useTranslation } from 'react-i18next';
 
 export default function FormSent() {
+
+    const { t } = useTranslation(["alert"]);
+
     return (
-        <ShowAlert message="Uw formulier is opgestuurd!" type="success" />
+        <ShowAlert type="success">
+            {t('Your form has been sent')}!
+        </ShowAlert>
     );
 }
