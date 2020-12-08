fae6bb1fc61437cfc3c9756c8e0365c6276aa7ee
JonasVervloet <vervloetjonas@hotmail.com>
2020-12-08 12:07:05 +0100

    Updates

---
+++ precache-manifest.96068586302b1268f09f4ae03048072f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3eff5d190df108067095e51fc4cb17c",
+    "url": "/solid-elections-submission/index.html"
+  },
+  {
+    "revision": "73f48d4b6787139cba78",
+    "url": "/solid-elections-submission/static/css/main.3fb8d33b.chunk.css"
+  },
+  {
+    "revision": "65cd5e42bc2680042609",
+    "url": "/solid-elections-submission/static/js/2.75950743.chunk.js"
+  },
+  {
+    "revision": "dd160c33b938c1abaf4f5e9ccdb0d9a1",
+    "url": "/solid-elections-submission/static/js/2.75950743.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73f48d4b6787139cba78",
+    "url": "/solid-elections-submission/static/js/main.25f689e0.chunk.js"
+  },
+  {
+    "revision": "b9792f7d8aea77c39cfb",
+    "url": "/solid-elections-submission/static/js/runtime-main.32217598.js"
+  }
+]);
